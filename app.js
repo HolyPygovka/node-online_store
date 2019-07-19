4ecdec778935efe286bf6f18fe522a122ea2f124
Roman <r2598509@gmail.com>
2019-07-19 12:20:02 +0300

    4 server without express

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ for (let i=0; i<a.length; i++) {
     console.log(a[i]);
 } */
 const http = require('http');
+const fs = require('fs');
 
-
-//http.createServer().listen(3000);
 http.createServer(function(request, response){
-    response.end('Hello my');
+    console.log(request.method);
+    /* console.log(request.headers('user-agent')); */
+    console.log(request.headers['user-agent']);
+
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    if (request.url == '/') {
+        response.end('Main <b>Page</b> Привет мир');
+    } else if (request.url == '/cat') {
+        response.end('Category <h2>Page</h2>');
+    } else if (request.url == '/dat') {
+        let myFile = fs.readFileSync('1.dat');
+        console.log(myFile);
+        response.end(myFile);
+    }
+    
 }).listen(3000);
