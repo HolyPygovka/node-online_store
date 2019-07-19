8dd7ee6f34a746f5cbc46c2fc0b24557e180a2e3
Roman <r2598509@gmail.com>
2019-07-19 16:15:14 +0300

    7 pug

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 let express = require('express');
-let app = express();
-/* publick - имя папки где храниться статика */
-app.use(express.static('public'));
+let app = express(); //создаю экземпляр экспресса
+
+app.use(express.static('public')); /* public - имя папки где храниться статика */
+
+/* Задаём шаблонизатор */
+app.set('view engine', 'pug');
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     console.log('load /')
-    res.render('index.html');
+    res.render('main.pug', {
+        foo: 4,
+        bar: 7
+    });
 });
