85320ebcef9bc1dd0124f3f3d2970f9c10621471
Roman <r2598509@gmail.com>
2019-07-19 12:47:57 +0300

    5 routing static

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-/* let a = [1,2,'Hello',6];
-console.log(a.length);
-for (let i=0; i<a.length; i++) {
-    console.log(a[i]);
-} */
-const http = require('http');
-const fs = require('fs');
+let express = require('express');
+let app = express();
+/* publick - имя папки где храниться статика */
+app.use(express.static('public'));
 
-http.createServer(function(request, response){
-    console.log(request.method);
-    /* console.log(request.headers('user-agent')); */
-    console.log(request.headers['user-agent']);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-
-    if (request.url == '/') {
-        response.end('Main <b>Page</b> Привет мир');
-    } else if (request.url == '/cat') {
-        response.end('Category <h2>Page</h2>');
-    } else if (request.url == '/dat') {
-        let myFile = fs.readFileSync('1.dat');
-        console.log(myFile);
-        response.end(myFile);
-    }
-    
-}).listen(3000);
+app.get('/', function(req, res){
+    console.log('load /')
+    res.render('index.html');
+});
