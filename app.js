dfc3e72a0094195b27c62d2d15f544aae042c7bf
Roman <r2598509@gmail.com>
2019-08-08 17:30:21 +0300

    15 order

--- app.js
+++ app.js
@@ -22,7 +22,31 @@ app.listen(3000, function () {
 });
 
 app.get('/', function (req, res) {
-  res.render('main.pug');
+  let cat = new Promise(function(resolve, reject){
+    con.query(
+      "SELECT id,name,cost,image,category FROM (SELECT id,name,cost,image,category, if(if(@curr_category != category, @curr_category := category, '') != '', @k := 0, @k := @k + 1) as ind  FROM goods, (SELECT @curr_category := '' ) v ) goods WHERE ind < 3",
+      function(error, result, fields){
+        if (error) reject(error);
+         resolve(result);
+      }
+    );
+  });
+  let catDescription = new Promise(function(resolve, reject){
+    con.query(
+      "SELECT * FROM category",
+      function(error, result, fields){
+        if (error) reject(error);
+         resolve(result);
+      }
+    );
+  });
+  Promise.all([cat, catDescription]).then(function (value){
+    console.log(value[1]);
+    res.render('index', {
+      goods: JSON.parse(JSON.stringify(value[0])),
+      cat: JSON.parse(JSON.stringify(value[1])),
+    });
+  });
 });
 
 app.get('/cat', function(req, res){
@@ -62,6 +86,10 @@ app.get('/goods', function(req, res){
   });
 });
 
+app.get('/order', function(req, res){
+  res.render('order');
+});
+
 app.post('/get-category-list', function(req, res){
   con.query('SELECT id, category FROM category', function(error, result, fields){
     if (error) throw error;
@@ -70,12 +98,34 @@ app.post('/get-category-list', function(req, res){
 });
 
 app.post('/get-goods-info', function(req, res){
-  con.query('SELECT id, name, cost FROM goods WHERE id IN('+req.body.key.join(',')+')', function(error, result, fields){
-    if (error) throw error;
-    let goods = {};
-    for (let i = 0; i < result.length; i++) {
-      goods[result[i]['id']] = result[i];
-    }
-    res.json(goods);
-  });
+  if (req.body.key.length !=0){
+    con.query('SELECT id, name, cost FROM goods WHERE id IN('+req.body.key.join(',')+')', function(error, result, fields){
+      if (error) throw error;
+      let goods = {};
+      for (let i = 0; i < result.length; i++) {
+        goods[result[i]['id']] = result[i];
+      }
+      res.json(goods);
+    });
+  } else {
+    res.send('0');
+  }
 });
+
+app.post('/finish-order', function(req, res){
+  console.log(req.body);
+  if(req.body.key.length != 0) {
+    let key = Object.keys(req.body.key);
+    con.query('SELECT id, name, cost FROM goods WHERE id IN('+key.join(',')+')', function(error, result, fields){
+      if (error) throw error;
+      console.log(result);
+      // sendMail(req.body, result).catch(console.error);
+      res.send('1');
+    });
+  } else {
+    res.send('0')
+  }
+});
+
+function sendMail(data, result){
+}
